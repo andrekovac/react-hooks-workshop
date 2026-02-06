36b3759db49deac5876fbd309b3793557e5e4035
André Kovac <me@andrekovac.com>
2026-02-06 17:08:25 +0700

    feat: Add React Compiler support with new Babel plugins and ESLint configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// React Compiler Configuration
+// Change return value to true to enable for entire project
+const ReactCompilerConfig = {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  sources: (_filename: string) => {
+    // Currently disabled - change to true to enable React Compiler
+    return false;
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+      },
+    }),
+  ],
+});
